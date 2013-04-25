f4ad565808714f3e25ce8a41365d8734f2844ea7
Justin Walgran <jwalgran@azavea.com>
2013-04-25 14:12:22 -0400

    Create nuget package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Azavea.Open.DAO.OleDb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OleDb adapter for FastDAO, an object-relational mapper for the .NET framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Azavea.Open.DAO.OleDb")]
+[assembly: AssemblyProduct("FastDAO")]
 // Configure log4net using the .config file
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
