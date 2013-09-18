e6cf7060fff3d0eb205aa061239b1e673f946b47
Justin Walgran <jwalgran@azavea.com>
2013-09-18 17:23:02 -0400

    1.0.12
    Previous version mistakenly built with x86 rather than Any CPU

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyFileVersion("1.0.11.0")]
+[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyFileVersion("1.0.12.0")]
 // Used for nuget packaging
-[assembly: AssemblyInformationalVersion("1.0.11")]
+[assembly: AssemblyInformationalVersion("1.0.12")]
